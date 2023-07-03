e0baf17ce04e6cdfe0f48ccd836c7a36001edc24
tepong32 <tepong32@gmail.com>
2023-07-03 13:56:45 +0800

    continue tutorial @ https://www.django-rest-framework.org/tutorial/2-requests-and-responses/ || watch tutorials.

--- try_shop/try_shop/settings.py
+++ try_shop/try_shop/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
 
 
     # custom
+    'snippets',
     # 'home',
 
 ]
+
 
 
 REST_FRAMEWORK = {
@@ -53,8 +55,11 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -138,3 +143,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH=False
